5671d3559a3a5a0f620b72d89821ba6832333e60
Niels Liisberg <nli@system-method.com>
2024-10-30 18:53:19 +0100

    CTE

--- 02-Common-table-expressions.sql
+++ 02-Common-table-expressions.sql
@@ -1,1 +1,15 @@
 -- https://www.ibm.com/docs/en/i/7.5?topic=queries-using-recursive-common-table-expressions-recursive-views
+
+-- This is what we can work with:
+select * from sqlxxl.systables;
+select * from sqlxxl.employee;
+
+-- Lets make a CTE
+with emp_full_name as (
+    select 
+        empno, 
+        rtrim(firstnme) concat ' ' concat midinit concat ' ' concat  lastname as full_name
+        from sqlxxl.employee
+)
+select * from emp_full_name
+where full_name like '%JOHN%';
