399116fd118e746ae9d7c565506848d1a7fd2806
Niels Liisberg <nli@system-method.com>
2024-11-12 18:19:51 +0100

    formatting

--- 16-Temporal-support.sql
+++ 16-Temporal-support.sql
@@ -1,16 +1,17 @@
 ﻿-- Examples of usages of the Db2 for i temporal support
 
-create or replace table sqlxxl.departmnt
-      (deptno    char(3)       not null,
-       deptname  varchar(36)   not null,
-       mgrno     char(6),
-       admrdept  char(3)       not null, 
-       location  char(16),
-       start_ts  timestamp(12) not null generated always as row begin,
-       end_ts    timestamp(12) not null generated always as row end,
-       ts_id     timestamp(12) generated always as transaction start id,
+create or replace table sqlxxl.departmnt(
+      deptno    char(3)       not null,
+      deptname  varchar(36)   not null,
+      mgrno     char(6),
+      admrdept  char(3)       not null, 
+      location  char(16),
+      start_ts  timestamp(12) not null generated always as row begin,
+      end_ts    timestamp(12) not null generated always as row end,
+      ts_id     timestamp(12) generated always as transaction start id,
       period system_time (start_ts, end_ts),
-      primary key (deptno));
+      primary key (deptno)
+);
       
 create or replace table sqlxxl.departmnt_hist like sqlxxl.departmnt;
 
@@ -22,18 +23,19 @@ select * from sqlxxl.departmnt for system_time as of current timestamp - 6 month
 
 alter table sqlxxl.departmnt drop versioning;
 
-create or replace table sqlxxl.departmnt
-      (deptno    char(3)       not null,
-       deptname  varchar(36)   not null,
-       mgrno     char(6),
-       admrdept  char(3)       not null, 
-       location  char(16),
-       turnover  dec(15 ,2),  -- now adding colum turnover  dec(15 ,2)!!!
-       start_ts  timestamp(12) not null generated always as row begin,
-       end_ts    timestamp(12) not null generated always as row end,
-       ts_id     timestamp(12) generated always as transaction start id,
+create or replace table sqlxxl.departmnt (
+      deptno    char(3)       not null,
+      deptname  varchar(36)   not null,
+      mgrno     char(6),
+      admrdept  char(3)       not null, 
+      location  char(16),
+      turnover  dec(15 ,2),  -- now adding colum turnover  dec(15 ,2)!!!
+      start_ts  timestamp(12) not null generated always as row begin,
+      end_ts    timestamp(12) not null generated always as row end,
+      ts_id     timestamp(12) generated always as transaction start id,
       period system_time (start_ts, end_ts),
-      primary key (deptno));
+      primary key (deptno)
+);
 
 create or replace table sqlxxl.departmnt_hist like department;
 
